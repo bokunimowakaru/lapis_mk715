730be44fdcad19468f6051c46df2b80ffa55c1de
bokunimowakaru <xbee@dream.jp>
2020-03-03 22:10:40 +0900

    ボード上のLEDのBLE制御

---
+++ cq_ex21_ble_led/cq_ex21_ble_led.c
@@ -0,0 +1,67 @@
+﻿/*******************************************************************************
+Lapis MK71511/MK71521用 サンプル・プログラム Example 21
+
+モジュール上のLEDの状態をBLEで遠隔制御します
+
+                                          Copyright (c) 2020 Wataru KUNINO
+                                          https://bokunimo.net/bokunimowakaru/
+*******************************************************************************/
+
+#include "./main.c"                               // main.cの組み込み
+
+static void cq_lbs_handler(uint8_t value){
+    if(value){
+        NRF_LOG_INFO("LED ON");
+        bsp_board_led_on(1);                    // LED5(GPIO P18)をON
+    }else{
+        NRF_LOG_INFO("LED OFF");
+        bsp_board_led_off(1);                   // LED5(GPIO P18)をOFF
+    }
+}
+
+void setup(){                                   // 起動時に1回だけ実行する関数
+    NRF_LOG_INFO("cq_ex21_ble_led");            // タイトルのシリアル出力
+    ble_stack_init();                           // BLEスタックを初期化
+    gap_params_init("cq_ex21_ble_led");         // BLEデバイス名を設定
+    gatt_init();
+    services_init();
+    advertising_init();
+    conn_params_init();
+    advertising_start();
+}
+
+void loop(){                                    // 繰り返し実行する関数
+    ;                                           // 何もしない
+}
+
+/*******************************************************************************
+動作例
+
+    <info> app_timer: RTC: initialized.
+    <info> app: (Started)
+    <info> app: cq_ex21_ble_led
+    <info> app: Connected
+    <info> app: conn_handle = 0, value = 1
+    <info> app: LED ON
+    <info> app: Disconnected
+    <info> app: Connected
+    <info> app: conn_handle = 0, value = 0
+    <info> app: LED OFF
+    <info> app: Disconnected
+
+********************************************************************************
+受信例
+
+    pi@raspberrypi:~ $ git clone http://github.com/bokunimowakaru/lapis_mk715
+    pi@raspberrypi:~ $ cd lapis_mk715/tools
+    pi@raspberrypi:~/lapis_mk715/tools $ sudo ./ble_nordic_led.py
+
+    Device xx:xx:xx:xx:xx:xx (random), RSSI=-49 dB, Connectable=True
+        9 Complete Local Name = cq_ex21_ble_led
+        LED = ON
+
+    Device xx:xx:xx:xx:xx:xx (random), RSSI=-58 dB, Connectable=True
+        9 Complete Local Name = cq_ex21_ble_led
+        LED = OFF
+
+*******************************************************************************/
