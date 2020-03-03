d771e2d1b2c0e2aaee43af6cd070ef6277b78444
Wataru KUNINO <xbee@dream.jp>
2020-03-03 22:07:34 +0900

    LED制御サンプルに対応

--- tools/ble_nordic_led.py
+++ tools/ble_nordic_led.py
@@ -20,7 +20,7 @@
 #   https://ianharvey.github.io/bluepy-doc/scanner.html
 #   https://ianharvey.github.io/bluepy-doc/peripheral.html
 
-target_device  = 'Nordic_Blinky'                        # ここに接続するデバイス名を入力
+target_device  = 'cq_ex21_ble_led'                      # ここに接続するデバイス名を入力
 target_service = '00001523-1212-efde-1523-785feabcd123' # ここに接続するサービスUUIDを入力
 interval = 5                                            # 動作間隔
 
@@ -50,7 +50,7 @@ while True:
         val = ''
         for (adtype, desc, value) in dev.getScanData():
             #print("  %3d %s = %s" % (adtype, desc, value))  # ad_t=[{8:'Short Local Name'},{9:'Complete Local Name'}]
-            if adtype == 9 and value == target_device:
+            if adtype == 9 and (value == target_device or value == 'Nordic_Blinky'):
                 isRohmMedal = value
             if adtype == 7 and value == target_service:
                 val = value
@@ -58,6 +58,7 @@ while True:
                 continue
         if isRohmMedal:
             print("\nDevice %s (%s), RSSI=%d dB, Connectable=%s" % (dev.addr, dev.addrType, dev.rssi, dev.connectable))
+            print("    9 Complete Local Name =",isRohmMedal)
             isRohmMedal = ''
             p = Peripheral(dev.addr, addrType='random')
             led = p.readCharacteristic(0x0010)
