c46357715c4a39da772d52072a9aca4a740999ce
Wataru KUNINO <xbee@dream.jp>
2020-03-22 11:59:16 +0900

    コメントの追加など

--- tools/ble_logger_basic.py
+++ tools/ble_logger_basic.py
@@ -25,80 +25,68 @@
 #   https://ianharvey.github.io/bluepy-doc/scanner.html
 #   https://www.rohm.co.jp/documents/11401/3946483/sensormedal-evk-002_ug-j.pdf
 
-interval = 1.01                     # 動作間隔
+interval = 1.01                                 # 動作間隔
 
-from bluepy import btle
-from sys import argv
-import getpass
-#from shutil import chown
-from time import sleep
+from bluepy import btle                         # bluepyからbtleを組み込む
+from time import sleep                          # timeからsleepを組み込む
 
-def payval(num, bytes=1, sign=False):
-    global val
-    a = 0
+def payval(num, bytes=1, sign=False):           # 受信データから値を抽出する
+    global val                                  # 受信データ用変数valを読み込む
+    a = 0                                       # 戻り値用変数aを定義する
     if num < 2 or len(val) < (num - 2 + bytes) * 2:
         print('ERROR: data length',len(val))
         return 0
-    for i in range(0, bytes):
+    for i in range(0, bytes):                   # バイト数分の値を変数aに代入
         a += (256 ** i) * int(val[(num - 2 + i) * 2 : (num - 1 + i) * 2],16)
-    if sign:
-        if a >= 2 ** (bytes * 8 - 1):
-            a -= 2 ** (bytes * 8)
-    return a
+    if sign:                                    # 符号つきの場合
+        if a >= 2 ** (bytes * 8 - 1):           # マイナス値のとき
+            a -= 2 ** (bytes * 8)               # マイナス値へ変換
+    return a                                    # 得られた値aを応答する
 
-def printval(dict, name, n, unit):
-    value = dict.get(name)
-    if value == None:
-        return
-    if type(value) is not str:
-        if n == 0:
-            value = round(value)
-        else:
-            value = round(value,n)
-    print('    ' + name + ' ' * (14 - len(name)) + '=', value, unit)
+def printval(dict, name, n, unit):              # 受信値を表示する関数
+    value = dict.get(name)                      # 変数dict内の項目nameの値を取得
+    if value == None:                           # 項目が無かったとき
+        return                                  # 戻る
+    if type(value) is not str:                  # 値が文字列で無かったとき
+        value = round(value,n)                  # 小数点以下第n位で丸める
+    print('    ' + name + ' ' * (14 - len(name)) + '=', value, unit)    # 表示
 
-scanner = btle.Scanner()
-
-while True:
-    try:
-        devices = scanner.scan(interval)
-    except Exception as e:
-        print("ERROR",e)
-        if getpass.getuser() != 'root':
-            print('使用方法: sudo', argv[0])
-            exit()
-        sleep(interval)
-        continue
-    sensors = dict()
-
-    for dev in devices:
-        isRohmMedal = ''
-        val = ''
+scanner = btle.Scanner()                        # インスタンスscannerを生成
+while True:                                     # 永久ループ
+    devices = scanner.scan(interval)            # BLEデバイスをスキャンする
+    for dev in devices:                         # 見つかった各デバイスについて
+        isRohmMedal = ''                        # 対象デバイス・フラグのクリア
+        val = ''                                # 取得値のクリア
         for (adtype, desc, value) in dev.getScanData():
+            # ビーコンがNordic nRF5かどうかを確認する
             if (adtype == 8 or adtype == 9) and (value  == 'nRF5x'):
                 isRohmMedal = 'Nordic nRF5'
             if desc == 'Manufacturer':
                 val = value
-            if isRohmMedal == '' or val == '':
+            if isRohmMedal != 'Nordic nRF5' or val == '':
                 continue
+            # Nordic nRF5 受信時の処理
             print("\nDevice %s (%s), RSSI=%d dB, Connectable=%s" % (dev.addr, dev.addrType, dev.rssi, dev.connectable))
-            sensors = dict()
             print('    isRohmMedal   =',isRohmMedal)
-            if isRohmMedal == 'Nordic nRF5':
-                sensors['ID'] = hex(payval(2,2))
-                sensors['Button'] = format(payval(6), '04b')
-                sensors['Temperature'] = -45 + 175 * payval(4,2) / 65536
-                sensors['Humidity'] = payval(7,2) / 65536 * 100
-                sensors['SEQ'] = payval(9)
-                sensors['RSSI'] = dev.rssi
-            isRohmMedal = ''
-
+            sensors = dict()                    # 辞書型変数sensorsの初期化
+            sensors['ID'] = hex(payval(2,2))    # ビーコン内のID(0x0059)を取得
+            sensors['Button'] = format(payval(6), '04b')    # ボタン状態を取得
+            sensors['Temperature'] = -45 + 175 * payval(4,2) / 65536    # 温度
+            sensors['Humidity'] = payval(7,2) / 65536 * 100 # 湿度を取得
+            sensors['SEQ'] = payval(9)          # 送信番号を取得
+            sensors['RSSI'] = dev.rssi          # RSSI(受信強度)を取得
+            printval(sensors, 'ID', 0, '')
+            printval(sensors, 'SEQ', 0, '')
+            printval(sensors, 'Button', 0, '')
+            printval(sensors, 'Temperature', 2, '℃')
+            printval(sensors, 'Humidity', 2, '%')
+            printval(sensors, 'RSSI', 0, 'dB')
 
 ''' 実行結果の一例
 pi@raspberrypi:~ $ cd
 pi@raspberrypi:~ $ git clone http://github.com/bokunimowakaru/lapis_mk715
 pi@raspberrypi:~ $ cd ~/lapis_mk715/tools
-pi@raspberrypi:~/lapis_mk715/tools $ sudo ./ble_logger_rohm.py
+pi@raspberrypi:~/lapis_mk715/tools $ sudo ./ble_logger_basic.py
 
 Device xx:xx:xx:xx:xx:xx (random), RSSI=-51 dB, Connectable=False
     isRohmMedal   = Nordic nRF5
