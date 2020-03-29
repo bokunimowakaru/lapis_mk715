9017076abfa6a5f4d942935fbe7efe8674063e30
Wataru KUNINO <xbee@dream.jp>
2020-03-29 13:52:13 +0900

    cq_ex22_ble_swサンプル対応。BLEハード異常時のメッセージ追加

--- tools/ble_nordic_sw.py
+++ tools/ble_nordic_sw.py
@@ -24,13 +24,15 @@
 #   本プログラムを作成するにあたり下記を参考にしました
 #   https://ianharvey.github.io/bluepy-doc/notifications.html
 
-target_device  = 'cq_ex21_ble_led'                      # ここに接続するデバイス名を入力
+target_device  = ['cq_ex21_ble_led','cq_ex22_ble_sw']   # ここに接続するデバイス名を入力
 target_service = '00001523-1212-efde-1523-785feabcd123' # ここに接続するサービスUUIDを入力
 
 from bluepy import btle
 from bluepy.btle import Peripheral, DefaultDelegate
+from time import sleep
 from sys import argv
 import getpass
+
 notified_val = b'\x00'
 
 class MyDelegate(DefaultDelegate):
@@ -50,6 +52,12 @@ scanner = btle.Scanner()
 while True:
     try:
         devices = scanner.scan(1)
+    except btle.BTLEManagementError as e:
+        print(e,'\nblupy管理ERRORにつき下記コマンドを実行してください')
+        print('sudo hciconfig hci0 down')
+        print('sudo hciconfig hci0 up')
+        print('sudo', argv[0])
+        exit()
     except Exception as e:
         print("ERROR",e)
         if getpass.getuser() != 'root':
@@ -65,7 +73,7 @@ while True:
         val = ''
         for (adtype, desc, value) in dev.getScanData():
             #print("  %3d %s = %s" % (adtype, desc, value))  # ad_t=[{8:'Short Local Name'},{9:'Complete Local Name'}]
-            if adtype == 9 and (value == target_device or value == 'Nordic_Blinky'):
+            if adtype == 9 and (value in target_device or value == 'Nordic_Blinky'):
                 isRohmMedal = value
             if adtype == 7 and value == target_service:
                 val = value
@@ -101,7 +109,8 @@ while True:
                     else:
                         led = b'\x01'
                         print('    LED = ON')
-                    p.writeCharacteristic(0x0010, led)
+                    # p.writeCharacteristic(0x0010, led)            # LED ON=1, OFF=0を送信する時
+                    p.writeCharacteristic(0x0010, notified_val)     # 受信した通知値を送信する
 
 ''' 実行結果の一例
 pi@raspberrypi:~ $ cd
