516d7641bf95b6d16407b62d435318021fb2a06b
bokunimowakaru <xbee@dream.jp>
2020-04-19 10:39:02 +0900

    低消費電力版・温湿度センサ

---
+++ cq_ex13_ble_hum_le/i2c_si7021.h
@@ -0,0 +1,21 @@
+#ifndef SI7021_H__
+#define SI7021_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "nrf_drv_twi.h"
+
+float getTemp();
+float getHum();
+void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context);
+void si7021Setup();
+void si7021Start();
+void si7021Stop();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SI7021_H__
